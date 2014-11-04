d27361af59b40dc09d9c2914044be4849a231a65
siewertserau <siewerts@erau.edu>
2014-11-03 19:57:17 -0700

    Updated test automation to be more clear.

--- raid_regression.sh
+++ raid_regression.sh
@@ -3,18 +3,24 @@
 # RAID Unit level regression script
 #
 echo "RAID UNIT REGRESSSION TEST"
+echo "RAID UNIT REGRESSSION TEST" >& testresults.log
 
 
-echo "checkout and build test"
+echo "TEST SET 1: checkout and build test"
+echo "TEST SET 1: checkout and build test" >>& testresults.log
 make clean
-make >& testresults.log
+make >>& testresults.log
 echo "" >>& testresults.log
 
-echo "simple RAID encode, erase and rebuild test"
+
+echo "TEST SET 2: simple RAID encode, erase and rebuild test"
+echo "TEST SET 2: simple RAID encode, erase and rebuild test" >>& testresults.log
 raidtest 1000 >>& testresults.log
 echo "" >>& testresults.log
 
-echo "REBUILD DIFF CHECK for Chunk4 rebuild" >>& testresults.log
+
+echo "TEST SET 3: REBUILD DIFF CHECK for Chunk4 rebuild"
+echo "TEST SET 3: REBUILD DIFF CHECK for Chunk4 rebuild" >>& testresults.log
 diff Chunk1.bin Chunk4_Rebuilt.bin >>& testresults.log
 diff Chunk2.bin Chunk4_Rebuilt.bin >>& testresults.log
 diff Chunk3.bin Chunk4_Rebuilt.bin >>& testresults.log
@@ -22,5 +28,7 @@ diff Chunk4.bin Chunk4_Rebuilt.bin >>& testresults.log
 echo "" >>& testresults.log
 
 
-echo "RAID performance test"
+echo "TEST SET 4: RAID performance test"
+echo "TEST SET 4: RAID performance test" >>& testresults.log
 raid_perftest 1000 >>& testresults.log
+echo "" >>& testresults.log
